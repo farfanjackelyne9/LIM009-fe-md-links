beea7484a1b8032402808705c00d85094b8581cb
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-06-14 12:08:23 -0500

    ordenando carpetas y agregando funcion de conversion relativa a absoluta

--- src/cli.js
+++ src/cli.js
@@ -3,17 +3,19 @@
 
 const [,,  ...args] = process.argv; 
 console.log(`link ingresado ${args}`); // Te pinta en la consola lo introducido.
+ 
 
-let path = require('path'); 
-
+let path = require('path');
 const route = `${args}`; //Constante que contiende la ruta introducida en la consola.
 
+//convierte ruta relativa en ruta absoluta.
 const routes = (route)=>{
-    if(path.isAbsolute(route) === true){
-        console.log("es absoluta");
-    }else{
-        console.log("es relativa");
+    if(path.isAbsolute(route) === false){
+        const absolute = path.resolve(route);
+        console.log(absolute);
     }
-
 }
 routes (route);
+
+
+
