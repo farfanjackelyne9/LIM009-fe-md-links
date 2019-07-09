5b863b3002690f654910389e382132b9ea2ffe1a
farfanjackelyne9 <farfanjackelyne9@gmail.com>
2019-07-09 14:47:52 -0500

    opciones de links con sus respectivas propiedades

--- src/READme.md
+++ src/READme.md
@@ -1,3 +1,3 @@
 [link1](https://babeljs.io/docs/en/next/babel-helper-wrap-function.html)
 [link2](https://babeljs.io/docs/en/next/babel-helpers.html)
-[link3](https://babeljs.io/docs/en/next/babel-helper-simple-access.html)
+[link3](https://neoattack.com/proyectos1/)
--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node 
 const [,,  ...args] = process.argv;
 const route =`${args}` //Constante que contiende la ruta introducida en la consola.
+
 const marked = require('marked');
 const path = require('path');
 const fs = require('fs');
+const fetch = require('node-fetch');
+
 
 
 //analiza ruta y devuelve si es absoluta.(T-F)-----------------------------------------
@@ -66,7 +69,7 @@ const analyzeMdFiles = (dir)=>{
                 mdFiles.push({file:absRoute});
             }
         }
-    }   
+    }
 }
 analyzeMdFiles(route);
 //console.log(mdFiles);
@@ -82,13 +85,53 @@ const convertToHTML=(mdFiles)=> {
         renderer.link = (href,title,text)=>{
             arrLink.push({href:href, text:text, file:x})   
         }
+        //convierte en html
         return marked(dir,{renderer});
     });
-    console.log(arrLink);
+    return arrLink;
 }
 convertToHTML(mdFiles);
 
 
 
+const arrObjLinks = (route) => {
+    const arrLinks = route ;
+    //console.log(arrLinks);
+    const objLinks = arrLinks.map((e) => {
+      const links = e.href;
+      return fetch(links)
+      .then((res) => {
+        e.status = res.status;
+        //console.log(res)
+        if (res.ok) {
+          e.statusText = 'ok';
+        } else {
+          e.statusText = 'fail';
+        }
+        return e;
+      }).catch((err) => {
+        e.ok = 'fail';
+        e.status = err.message;
+        return e;
+      });
+    });
+    return Promise.all(objLinks);
+  };
+  arrObjLinks(convertToHTML(mdFiles))
+  //.then((res)=>{console.log(res)})
+
   
-   
+  const stateLinks = (route) => {
+    const promiseArrObjLinks = route.then((res)=>{
+     const allLinks = res.map ((e)=>{e.href
+     });
+     const links = [...new Set(allLinks)];
+     const failLinks = res.filter((e)=> e.statusText === 'fail')
+     console.log({
+         total:res.length,
+         unique:links.length,
+         broken:failLinks.length,
+     })
+    })
+  };
+  stateLinks(arrObjLinks(convertToHTML(mdFiles)))
